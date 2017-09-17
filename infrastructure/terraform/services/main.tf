aabd1ed6d0755108576d42bd5588c4d8b22249c2
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-09-17 21:35:47 +0100

    Add nginx service and task

--- infrastructure/terraform/services/main.tf
+++ infrastructure/terraform/services/main.tf
@@ -20,8 +20,8 @@ provider "template" {
 # ECS Services and Tasks
 ##############################################################################
 
-resource "aws_iam_role" "server_role" {
-  name               = "Service"
+resource "aws_iam_role" "service_role" {
+  name = "service-role"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -39,9 +39,9 @@ resource "aws_iam_role" "server_role" {
 POLICY
 }
 
-resource "aws_iam_role_policy" "server_role_policy" {
-  name = "ServicePolicy"
-  role = "${aws_iam_role.server_role.id}"
+resource "aws_iam_role_policy" "service_role_policy" {
+  name = "service-role-policy"
+  role = "${aws_iam_role.service_role.id}"
 
   policy = <<EOF
 {
@@ -56,55 +56,7 @@ resource "aws_iam_role_policy" "server_role_policy" {
                 "elasticloadbalancing:DeregisterTargets",
                 "elasticloadbalancing:Describe*",
                 "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-                "elasticloadbalancing:RegisterTargets",
-                "ecr:GetAuthorizationToken",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_role" "container_role" {
-  name               = "Container"
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ecs.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy" "container_role_policy" {
-  name = "ContainerPolicy"
-  role = "${aws_iam_role.container_role.id}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:AuthorizeSecurityGroupIngress",
-                "ec2:Describe*",
-                "ecr:GetAuthorizationToken",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage"
+                "elasticloadbalancing:RegisterTargets"
             ],
             "Resource": "*"
         }
@@ -114,7 +66,7 @@ EOF
 }
 
 resource "aws_iam_role" "container_tasks_role" {
-  name               = "ContainerTasks"
+  name = "container-tasks-role"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -126,6 +78,14 @@ resource "aws_iam_role" "container_tasks_role" {
       },
       "Effect": "Allow",
       "Sid": ""
+    },
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
   ]
 }
@@ -133,7 +93,7 @@ POLICY
 }
 
 resource "aws_iam_role_policy" "container_tasks_role_policy" {
-  name = "ContainerTasksPolicy"
+  name = "container-tasks-role-policy"
   role = "${aws_iam_role.container_tasks_role.id}"
 
   policy = <<EOF
@@ -149,6 +109,13 @@ resource "aws_iam_role_policy" "container_tasks_role_policy" {
                 "ecr:BatchGetImage"
             ],
             "Resource": "*"
+        },
+        {
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::${data.terraform_remote_state.ecs.ecs-cluster-bucket-name}/*"
         }
     ]
 }
@@ -191,16 +158,24 @@ data "template_file" "web_template" {
   }
 }
 
+data "template_file" "nginx_template" {
+  template = "${file("task-definitions/nginx.json")}"
+
+  vars {
+    account_id         = "${var.account_id}"
+    bucket_name        = "${data.terraform_remote_state.ecs.ecs-cluster-bucket-name}"
+  }
+}
+
 resource "aws_ecs_service" "accounts" {
   name            = "accounts"
   cluster         = "${data.terraform_remote_state.ecs.ecs-cluster-id}"
   task_definition = "${aws_ecs_task_definition.accounts.arn}"
   desired_count   = 1
-  depends_on      = ["aws_iam_role_policy.container_role_policy"]
 
   placement_strategy {
-    type  = "spread"
-    field = "instanceId"
+    type  = "binpack"
+    field = "cpu"
   }
 
   placement_constraints {
@@ -214,6 +189,8 @@ resource "aws_ecs_task_definition" "accounts" {
   container_definitions = "${data.template_file.accounts_template.rendered}"
   task_role_arn         = "${aws_iam_role.container_tasks_role.arn}"
 
+  depends_on      = ["aws_iam_role_policy.container_tasks_role_policy"]
+
   placement_constraints {
     type       = "memberOf"
     expression = "${format("attribute:ecs.availability-zone in [%sa, %sb]", var.aws_region, var.aws_region)}"
@@ -225,7 +202,6 @@ resource "aws_ecs_service" "designs" {
   cluster         = "${data.terraform_remote_state.ecs.ecs-cluster-id}"
   task_definition = "${aws_ecs_task_definition.designs.arn}"
   desired_count   = 1
-  depends_on      = ["aws_iam_role_policy.container_role_policy"]
 
   placement_strategy {
     type  = "spread"
@@ -243,6 +219,8 @@ resource "aws_ecs_task_definition" "designs" {
   container_definitions = "${data.template_file.designs_template.rendered}"
   task_role_arn         = "${aws_iam_role.container_tasks_role.arn}"
 
+  depends_on      = ["aws_iam_role_policy.container_tasks_role_policy"]
+
   placement_constraints {
     type       = "memberOf"
     expression = "${format("attribute:ecs.availability-zone in [%sa, %sb]", var.aws_region, var.aws_region)}"
@@ -254,7 +232,6 @@ resource "aws_ecs_service" "auth" {
   cluster         = "${data.terraform_remote_state.ecs.ecs-cluster-id}"
   task_definition = "${aws_ecs_task_definition.auth.arn}"
   desired_count   = 1
-  depends_on      = ["aws_iam_role_policy.container_role_policy"]
 
   placement_strategy {
     type  = "spread"
@@ -272,6 +249,8 @@ resource "aws_ecs_task_definition" "auth" {
   container_definitions = "${data.template_file.auth_template.rendered}"
   task_role_arn         = "${aws_iam_role.container_tasks_role.arn}"
 
+  depends_on      = ["aws_iam_role_policy.container_tasks_role_policy"]
+
   placement_constraints {
     type       = "memberOf"
     expression = "${format("attribute:ecs.availability-zone in [%sa, %sb]", var.aws_region, var.aws_region)}"
@@ -283,19 +262,10 @@ resource "aws_ecs_service" "web" {
   cluster         = "${data.terraform_remote_state.ecs.ecs-cluster-id}"
   task_definition = "${aws_ecs_task_definition.server.arn}"
   desired_count   = 1
-  depends_on      = ["aws_iam_role_policy.server_role_policy"]
 
   placement_strategy {
     type  = "spread"
     field = "instanceId"
-  }
-
-  iam_role        = "${aws_iam_role.server_role.arn}"
-
-  load_balancer {
-    elb_name       = "${data.terraform_remote_state.ecs.ecs-cluster-elb-name}"
-    container_name = "web"
-    container_port = 8080
   }
 
   placement_constraints {
@@ -308,6 +278,46 @@ resource "aws_ecs_task_definition" "server" {
   family                = "web"
   container_definitions = "${data.template_file.web_template.rendered}"
   task_role_arn         = "${aws_iam_role.container_tasks_role.arn}"
+
+  depends_on      = ["aws_iam_role_policy.container_tasks_role_policy"]
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "${format("attribute:ecs.availability-zone in [%sa, %sb]", var.aws_region, var.aws_region)}"
+  }
+}
+
+resource "aws_ecs_service" "nginx" {
+  name            = "nginx"
+  cluster         = "${data.terraform_remote_state.ecs.ecs-cluster-id}"
+  task_definition = "${aws_ecs_task_definition.nginx.arn}"
+  desired_count   = 1
+
+  iam_role        = "${aws_iam_role.service_role.arn}"
+
+  load_balancer {
+    elb_name       = "${data.terraform_remote_state.ecs.ecs-cluster-elb-name}"
+    container_name = "nginx"
+    container_port = 443
+  }
+
+  placement_strategy {
+    type  = "spread"
+    field = "instanceId"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "${format("attribute:ecs.availability-zone in [%sa, %sb]", var.aws_region, var.aws_region)}"
+  }
+}
+
+resource "aws_ecs_task_definition" "nginx" {
+  family                = "nginx"
+  container_definitions = "${data.template_file.nginx_template.rendered}"
+  task_role_arn         = "${aws_iam_role.container_tasks_role.arn}"
+
+  depends_on      = ["aws_iam_role_policy.container_tasks_role_policy"]
 
   placement_constraints {
     type       = "memberOf"
@@ -380,4 +390,25 @@ resource "aws_s3_bucket_object" "truststore-server" {
   key    = "environments/production/keystores/truststore-server.jks"
   source = "environments/production/keystores/truststore-server.jks"
   etag   = "${md5(file("environments/production/keystores/truststore-server.jks"))}"
+}
+
+resource "aws_s3_bucket_object" "nginx-certificate" {
+  bucket = "${data.terraform_remote_state.ecs.ecs-cluster-bucket-name}"
+  key    = "environments/production/nginx/nginx.crt"
+  source = "environments/production/nginx/nginx.crt"
+  etag   = "${md5(file("environments/production/nginx/nginx.crt"))}"
+}
+
+resource "aws_s3_bucket_object" "nginx-private-key" {
+  bucket = "${data.terraform_remote_state.ecs.ecs-cluster-bucket-name}"
+  key    = "environments/production/nginx/nginx.key"
+  source = "environments/production/nginx/nginx.key"
+  etag   = "${md5(file("environments/production/nginx/nginx.key"))}"
+}
+
+resource "aws_s3_bucket_object" "nginx-config" {
+  bucket = "${data.terraform_remote_state.ecs.ecs-cluster-bucket-name}"
+  key    = "environments/production/config/nginx.conf"
+  source = "environments/production/config/nginx.conf"
+  etag   = "${md5(file("environments/production/config/nginx.conf"))}"
 }

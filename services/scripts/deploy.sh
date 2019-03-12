55224f2f3212f6dbb83b43fb877e06a795ed0622
Andrea Medeghini <andrea@nextbreakpoint.com>
2019-03-12 07:47:40 +0000

    upgrade to java 11

--- services/scripts/deploy.sh
+++ services/scripts/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-mvn -pl common clean install && mvn clean install && mvn docker:build docker:push -Ddocker.registry=$1 -Ddocker.username=$2 -Ddocker.password=$3
+pushd common
+
+mvn clean install
+
+popd
+
+mvn clean install
+
+mvn docker:build docker:push -Ddocker.registry=$1 -Ddocker.username=$2 -Ddocker.password=$3

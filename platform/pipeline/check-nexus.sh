bda4cccadf1e3ccc821599f871db2b3c1dc19af8
Andrea Medeghini <andrea@nextbreakpoint.com>
2022-02-17 19:03:19 +0000

    fix timeout

--- platform/pipeline/check-nexus.sh
+++ platform/pipeline/check-nexus.sh
@@ -5,7 +5,7 @@ declare -i n=1
 delay=5
 attempts=60
 
-until bash -c 'docker exec -it $(docker container ls -f name=pipeline-nexus-1 -q) cat /opt/sonatype/sonatype-work/nexus3/admin.password'; do echo "Waiting for Nexus..."; sleep $delay; n=$n+1; if [ $n -eq $attempts ]; then exit 1; fi; done
+until bash -c 'docker exec $(docker container ls -f name=pipeline-nexus-1 -q) cat /opt/sonatype/sonatype-work/nexus3/admin.password'; do echo "Waiting for Nexus..."; sleep $delay; n=$n+1; if [ $n -eq $attempts ]; then exit 1; fi; done
 
 echo "Nexus ready"
 

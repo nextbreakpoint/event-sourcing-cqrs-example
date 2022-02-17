0ebf3ce51b052d7816259b16dedcda66d540fb67
Andrea Medeghini <andrea@nextbreakpoint.com>
2022-02-17 18:16:51 +0000

    fix timeout

--- platform/pipeline/check-nexus.sh
+++ platform/pipeline/check-nexus.sh
@@ -2,8 +2,8 @@
 
 declare -i n=1
 
-delay=1
-attempts=5
+delay=5
+attempts=30
 
 until bash -c 'docker exec -it $(docker container ls -f name=pipeline-nexus-1 -q) cat /opt/sonatype/sonatype-work/nexus3/admin.password >> /dev/null'; do echo "Waiting for Nexus..."; sleep $delay; n=$n+1; if [ $n -eq $attempts ]; then exit 1; fi; done
 

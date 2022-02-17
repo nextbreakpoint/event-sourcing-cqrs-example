51e644db93c0f4bc371f70308bb42f1418b33ca6
Andrea Medeghini <andrea@nextbreakpoint.com>
2022-02-17 19:17:48 +0000

    fix pipeline

--- platform/pipeline/configure-nexus.sh
+++ platform/pipeline/configure-nexus.sh
@@ -8,14 +8,7 @@ if [ $? -eq 0 ]
 
 then
 
-echo "Create repository"
-
-export NEXUS_HOST=localhost
-export NEXUS_PORT=38081
-export NEXUS_USERNAME=admin
-export NEXUS_PASSWORD=$(pipeline/get-nexus-password.sh)
-
-curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X POST "http://${NEXUS_HOST}:${NEXUS_PORT}/service/rest/v1/repositories/maven/hosted" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"name\": \"maven-internal\", \"online\": true, \"storage\": { \"blobStoreName\": \"default\", \"strictContentTypeValidation\": true, \"writePolicy\": \"allow_once\" }, \"cleanup\": { \"policyNames\": [ \"string\" ] }, \"component\": { \"proprietaryComponents\": true }, \"maven\": { \"versionPolicy\": \"MIXED\", \"layoutPolicy\": \"STRICT\" }}"
+pipeline/create-repository.sh
 
 else
 

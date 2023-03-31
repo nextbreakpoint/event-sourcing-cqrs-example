13317f19e49c8ea65ef8af03665198135b39c40e
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-03-31 16:03:31 +0100

    extract services secrets

--- scripts/helm-install-services.sh
+++ scripts/helm-install-services.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-helm upgrade --install service-authentication services/authentication/helm -n services --set image.repository=integration/authentication,image.tag=${VERSION},replicas=1,clientDomain=$(minikube ip),clientWebUrl=https://$(minikube ip):443,clientAuthUrl=https://$(minikube ip):443,enableDebug=false,loggingLevel=${LOGGING_LEVEL}
+helm upgrade --install service-authentication services/authentication/helm -n services --set image.repository=integration/authentication,image.tag=${VERSION},replicas=1,clientDomain=$(minikube ip),clientWebUrl=https://$(minikube ip),clientAuthUrl=https://$(minikube ip),enableDebug=false,loggingLevel=${LOGGING_LEVEL}
 
 helm upgrade --install service-accounts services/accounts/helm -n services --set image.repository=integration/accounts,image.tag=${VERSION},replicas=1,clientDomain=$(minikube ip),enableDebug=false,loggingLevel=${LOGGING_LEVEL}
 
@@ -14,4 +14,4 @@ helm upgrade --install service-designs-render services/designs-render/helm -n se
 
 helm upgrade --install service-gateway services/gateway/helm -n services --set image.repository=integration/gateway,image.tag=${VERSION},replicas=1,clientDomain=$(minikube ip),enableDebug=false,loggingLevel=${LOGGING_LEVEL}
 
-helm upgrade --install service-frontend services/frontend/helm -n services --set image.repository=integration/frontend,image.tag=${VERSION},replicas=1,clientWebUrl=https://$(minikube ip):443,clientApiUrl=https://$(minikube ip):443,enableDebug=false,loggingLevel=${LOGGING_LEVEL}
+helm upgrade --install service-frontend services/frontend/helm -n services --set image.repository=integration/frontend,image.tag=${VERSION},replicas=1,clientWebUrl=https://$(minikube ip),clientApiUrl=https://$(minikube ip),enableDebug=false,loggingLevel=${LOGGING_LEVEL}

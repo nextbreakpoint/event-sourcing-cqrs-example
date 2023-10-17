12d6f38da4d06cc53f9079b118f637d45f3abfe4
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-10-17 23:11:21 +0100

    fix signout endpoint

---
+++ services/authentication/src/main/java/com/nextbreakpoint/blueprint/authentication/handlers/GitHubSignOutScope.java
@@ -0,0 +1,18 @@
+package com.nextbreakpoint.blueprint.authentication.handlers;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.core.http.Cookie;
+import io.vertx.rxjava.ext.auth.User;
+import io.vertx.rxjava.ext.web.RoutingContext;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(setterPrefix = "with", toBuilder = true)
+public class GitHubSignOutScope {
+    private RoutingContext routingContext;
+    private User user;
+    private String redirectTo;
+    private Cookie cookie;
+}

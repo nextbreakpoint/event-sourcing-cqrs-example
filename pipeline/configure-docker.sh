7ebb243bff95b0c93af61a6f5f5c18d38cae883f
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-10-23 23:46:43 +0100

    fix pipeline

---
+++ pipeline/configure-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+sudo echo 127.0.0.1 host.docker.internal >> /etc/hosts
--- pipeline/setup.sh
+++ pipeline/setup.sh
@@ -9,4 +9,3 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.adoptium.net/artifact
 sudo apt-get update -y && sudo apt-get install -y ca-certificates curl gnupg lsb-release temurin-17-jdk maven docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER && newgrp docker
 
-sudo echo 127.0.0.1 host.docker.internal >> /etc/hosts

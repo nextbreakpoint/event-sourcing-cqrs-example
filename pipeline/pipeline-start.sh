fd7c7d92766116b296baa05fc138d6d8a7406b85
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-10-23 22:27:21 +0100

    fix pipeline

--- pipeline/pipeline-start.sh
+++ pipeline/pipeline-start.sh
@@ -3,6 +3,5 @@
 set -e
 
 docker compose -f docker-compose-pipeline.yaml -p pipeline up -d
-docker compose -f docker-compose-platform.yaml -p platform up -d
 
 docker ps
--- pipeline/pipeline-stop.sh
+++ pipeline/pipeline-stop.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-docker compose -f docker-compose-platform.yaml -p platform down
 docker compose -f docker-compose-pipeline.yaml -p pipeline down

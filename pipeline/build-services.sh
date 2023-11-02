45c25068a5b77673e42c5c9ab9eb46b25d67e086
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-11-02 09:25:36 +0000

    enable use platform

--- pipeline/build-services.sh
+++ pipeline/build-services.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-./scripts/build-services.sh --docker-host="172.17.0.1" --nexus-password=$(./pipeline/get-nexus-password.sh) --quiet
+./scripts/build-services.sh --docker-host="172.17.0.1" --nexus-password=$(./pipeline/get-nexus-password.sh) --quiet --use-platform
 

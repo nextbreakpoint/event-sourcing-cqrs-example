c9d6a482e0352acd6319784976933d1a529229cc
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-11-02 08:34:05 +0000

    fix issue with pact tests not working in github action

--- services/authentication/src/test/java/com/nextbreakpoint/blueprint/authentication/PactConsumerTests.java
+++ services/authentication/src/test/java/com/nextbreakpoint/blueprint/authentication/PactConsumerTests.java
@@ -7,6 +7,7 @@ import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.junit.MockServerConfig;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.consumer.model.MockServerImplementation;
 import au.com.dius.pact.core.model.PactSpecVersion;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
@@ -19,6 +20,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
@@ -49,7 +52,6 @@ public class PactConsumerTests {
   private final int expectedPort = 8000;
   private final String expectedHost = "localhost";
   private final String expectedProtocol = "http";
-
 
   @BeforeAll
   public static void before() {
@@ -155,8 +157,9 @@ public class PactConsumerTests {
   }
 
   @Test
+  @EnabledOnOs(OS.MAC)
   @PactTestFor(providerName = "accounts", pactMethod = "accountDoesNotExist", pactVersion = PactSpecVersion.V4)
-  @MockServerConfig(providerName = "accounts", port = "39001")
+  @MockServerConfig(providerName = "accounts", port = "39001", implementation = MockServerImplementation.KTorServer)
   @DisplayName("should create an account and redirect to designs when authenticated user doesn't have an account")
   public void shouldCreateAnAccountAndRedirectToDesignsWhenAuthenticatedUserDoNotHaveAnAccount(MockServer mockServer) throws IOException {
     whenHttp(githubStub)
@@ -184,8 +187,9 @@ public class PactConsumerTests {
   }
 
   @Test
+  @EnabledOnOs(OS.MAC)
   @PactTestFor(providerName = "accounts", pactMethod = "accountExists", pactVersion = PactSpecVersion.V4)
-  @MockServerConfig(providerName = "accounts", port = "39001")
+  @MockServerConfig(providerName = "accounts", port = "39001", implementation = MockServerImplementation.KTorServer)
   @DisplayName("should not create an account and redirect to designs when authenticated user already has an account")
   public void shouldNotCreateAnAccountAndRedirectToDesignsWhenAuthenticatedUserAlreadyHasAnAccount(MockServer mockServer) throws IOException {
     whenHttp(githubStub)
@@ -206,5 +210,23 @@ public class PactConsumerTests {
     verifyHttp(githubStub).once(post(TestConstants.OAUTH_TOKEN_PATH), withHeader("accept", "application/json,application/x-www-form-urlencoded;q=0.9"))
             .then().once(get(TestConstants.OAUTH_USER_EMAILS_PATH), withHeader("authorization", "Bearer abcdef"))
             .then().never(get(TestConstants.OAUTH_USER_PATH));
+  }
+
+  @Test
+  @EnabledOnOs(OS.LINUX)
+  @PactTestFor(providerName = "accounts", pactMethod = "accountDoesNotExist", pactVersion = PactSpecVersion.V4)
+  @MockServerConfig(providerName = "accounts", port = "39001", hostInterface = "172.17.0.1", implementation = MockServerImplementation.KTorServer)
+  @DisplayName("should create an account and redirect to designs when authenticated user doesn't have an account")
+  public void shouldCreateAnAccountAndRedirectToDesignsWhenAuthenticatedUserDoNotHaveAnAccountWorkaround(MockServer mockServer) throws IOException {
+    shouldCreateAnAccountAndRedirectToDesignsWhenAuthenticatedUserDoNotHaveAnAccount(mockServer);
+  }
+
+  @Test
+  @EnabledOnOs(OS.LINUX)
+  @PactTestFor(providerName = "accounts", pactMethod = "accountExists", pactVersion = PactSpecVersion.V4)
+  @MockServerConfig(providerName = "accounts", port = "39001", hostInterface = "172.17.0.1", implementation = MockServerImplementation.KTorServer)
+  @DisplayName("should not create an account and redirect to designs when authenticated user already has an account")
+  public void shouldNotCreateAnAccountAndRedirectToDesignsWhenAuthenticatedUserAlreadyHasAnAccountWorkaround(MockServer mockServer) throws IOException {
+    shouldNotCreateAnAccountAndRedirectToDesignsWhenAuthenticatedUserAlreadyHasAnAccount(mockServer);
   }
 }

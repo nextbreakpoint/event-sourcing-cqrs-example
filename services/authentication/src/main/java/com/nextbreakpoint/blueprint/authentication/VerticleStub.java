a0febcbfdfc50df54e4a4ac99aa57fccf482e9b0
Andrea Medeghini <andrea@nextbreakpoint.com>
2023-12-17 23:22:09 +0000

    refactor

--- services/authentication/src/main/java/com/nextbreakpoint/blueprint/authentication/VerticleStub.java
+++ services/authentication/src/main/java/com/nextbreakpoint/blueprint/authentication/VerticleStub.java
@@ -4,12 +4,14 @@ import com.nextbreakpoint.blueprint.authentication.common.AccountsClient;
 import com.nextbreakpoint.blueprint.authentication.common.GitHubClient;
 import com.nextbreakpoint.blueprint.authentication.common.OAuthAdapter;
 import com.nextbreakpoint.blueprint.authentication.common.RoutingContextAdapter;
+import com.nextbreakpoint.blueprint.authentication.common.RoutingContextHandlerAdapter;
 import com.nextbreakpoint.blueprint.authentication.common.TokenProvider;
 import com.nextbreakpoint.blueprint.authentication.handlers.GitHubSignInHandler;
 import com.nextbreakpoint.blueprint.authentication.handlers.GitHubSignInScope;
 import io.vertx.core.Handler;
 import io.vertx.core.Launcher;
 import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.ext.web.RoutingContext;
 import rx.Single;
 
 public class VerticleStub extends Verticle {
@@ -20,8 +22,8 @@ public class VerticleStub extends Verticle {
     }
 
     @Override
-    protected Handler<RoutingContextAdapter> createSignInHandler(String cookieDomain, GitHubClient githubClient, AccountsClient accountsClient, TokenProvider tokenProvider, OAuthAdapter oauthAdapter) {
-        return new GitHubSignInHandler(cookieDomain, githubClient, accountsClient, tokenProvider, oauthAdapter) {
+    protected Handler<RoutingContext> createSignInHandler(String webUrl, String cookieDomain, GitHubClient githubClient, AccountsClient accountsClient, TokenProvider tokenProvider, OAuthAdapter oauthAdapter) {
+        return new RoutingContextHandlerAdapter(webUrl, new GitHubSignInHandler(cookieDomain, githubClient, accountsClient, tokenProvider, oauthAdapter) {
             @Override
             public void handle(RoutingContextAdapter routingContext) {
                 handleAuthenticatedAccess(routingContext);
@@ -42,6 +44,6 @@ public class VerticleStub extends Verticle {
             protected Single<JsonObject> fetchUserInfo(GitHubSignInScope scope) {
                 return Single.just(new JsonObject().put("name", "Micky Mouse"));
             }
-        };
+        });
     }
 }

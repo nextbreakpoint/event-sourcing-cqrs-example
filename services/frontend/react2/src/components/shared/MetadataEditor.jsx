37b9d42be9fba9f70a4685281bbe41d4997743dd
Andrea Medeghini <andrea@nextbreakpoint.com>
2024-03-12 17:28:06 +0000

    modernise ui

---
+++ services/frontend/react2/src/components/shared/MetadataEditor.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState, useCallback } from 'react';
+
+import { Editor, EditorState, ContentState, CompositeDecorator } from 'draft-js'
+
+const KEYWORD_REGEXP = /translation|rotation|scale|point|julia|options/g
+
+function findWithRegex(regex, contentBlock, callback) {
+  const text = contentBlock.getText();
+  let matchArr, start;
+  while ((matchArr = regex.exec(text)) !== null) {
+    start = matchArr.index;
+    callback(start, start + matchArr[0].length);
+  }
+}
+
+function keywordStrategy(contentBlock, callback, contentState) {
+  findWithRegex(KEYWORD_REGEXP, contentBlock, callback);
+}
+
+const KeywordSpan = (props) => {
+  return <span class="keyword">{props.children}</span>
+}
+
+const compositeDecorator = new CompositeDecorator([
+  {
+    strategy: keywordStrategy,
+    component: KeywordSpan
+  }
+])
+
+export default function MetadataEditor({ initialValue, readOnly, onContentChanged }) {
+    const [ editorState, setEditorState ] = useState(() => EditorState.createWithContent(ContentState.createFromText(initialValue), compositeDecorator))
+
+    const onChangeCallback = useCallback((editorState) => {
+        setEditorState(editorState)
+        if (onContentChanged) {
+            onContentChanged(editorState.getCurrentContent().getPlainText())
+        }
+    })
+
+    return (
+      <div class="editor">
+        <Editor readOnly={readOnly} editorState={editorState} onChange={onChangeCallback}/>
+      </div>
+    )
+}
